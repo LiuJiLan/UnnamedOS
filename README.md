e2310af98c2bbfb088bd00166b7c841292cf0871
LiuDongChen <ldc31415926@126.com>
2022-01-11 15:02:27 +0800

    kernel part

--- README.md
+++ README.md
@@ -10,4 +10,29 @@ $ sudo apt update
 $ sudo apt install build-essential gcc make perl dkms git gcc-riscv64-unknown-elf gdb-multiarch qemu-system-misc
 ```
 
-去年用的riscv64-unknown-elf-gdb但由于要源码编译就暂时懒得用了。
+注意: apt安装的qemu版本只有4.几
+
+## 安装 riscv64-unknown-elf-gdb
+
+可以参考:
+https://rcore-os.github.io/rCore-Tutorial-deploy/docs/pre-lab/gdb.html
+
+## 安装 riscv64-unknown-elf-gdb
+
+可以参考:
+https://rcore-os.github.io/rCore_tutorial_doc/chapter2/part5.html
+此处我选择了最新的stable版的qemu-6.1.1
+
+另外由于Maix Dock的k210的特权级版本是1.9, 所以涉及到一些处理, 可以参考
+https://github.com/riscvarchive/riscv-qemu/wiki
+
+### 本地编译出的问题:
+
+```ERROR: Cannot find Ninja```
+解决方法: ```sudo apt install ninja-build```
+
+```ERROR: glib-2.56 gthread-2.0 is required to compile QEMU```
+https://blog.csdn.net/fuxy3/article/details/104732541
+
+
+
---
+++ code/kernel/memlayout.h
@@ -0,0 +1,52 @@
+//
+//  memlayout.h
+//  xv6-qemu
+//
+//  Created by LiuJiLan on 2022/1/9.
+//
+
+//  EXTMEM是xv6中内核所在的物理地址,
+//  同时也是xv6中所使用平台的实际内存的映射地址,
+//  相当于SRAM映射到内存的那个起始地址0x80000000U
+
+//  KERNBASE是xv6中内核在虚拟内存中的起始位置
+//  我当时很好奇为什么需要再多加一个KERNLINK的内核链接地址
+//  原因:
+//  xv6中把实际物理地址0x0-0x100000中的base memory和I/O space也当做了内核的一部分
+//  这部分和真正的“内核代码”一起被映射到了KERNBASE
+//  所以KERNLINK是xv6中“内核代码”运行的地方
+
+//  我的虚拟内存映射参考了非教学版 rCore 的虚拟地址和物理地址的映射关系
+//  https://rcore-os.github.io/rCore-Tutorial-deploy/docs/lab-3/guide/part-1.html
+//  在我的代码的意义里, KERNBASE和KERNLINK都是同一个值, 都是0xFFFF_FFFF_C020_0000
+
+#ifndef memlayout_h
+#define memlayout_h
+
+//#define EXTMEM  0x100000            // Start of extended memory
+//#define PHYSTOP 0xE000000           // Top physical memory
+//#define DEVSPACE 0xFE000000         // Other devices are at high addresses
+
+//#define KERNBASE 0x80000000         // First kernel virtual address
+//#define KERNLINK (KERNBASE+EXTMEM)  // Address where kernel is linked
+
+//  命名暂时与xv6保持一致, 之后再换名字
+#define EXTMEM  0x80000000U
+//  rCore把它放在了0x80200000
+//  巧合的是我们内核所在的物理地址跟xv6中的虚拟地址竟是一个值, 时代在进步
+
+#define KERNBASE 0xFFFFFFFFC0200000U    //  参考rCore
+#define KERNLINK KERNBASE               //  注意链接脚本要和这里的值一致
+
+#define V_P_DIFF 0xFFFFFFFF40200000U
+//  xv6中由于设计上的巧合导致虚拟内存和物理内存的差值正好是KERNBASE
+//  我们这里额外定义一个虚拟内存和物理内存的差值, 通过KERNBASE-EXTMEM得出
+
+#define V2P(a) (((unsigned long) (a)) - V_P_DIFF)   //  64位下改了数据类型
+#define P2V(a) ((void *)(((unsigned char *) (a)) + V_P_DIFF))
+
+//  纯数值的转换
+#define V2P_WO(x) ((x) - V_P_DIFF)    // same as V2P, but without casts //  不带类型转换
+#define P2V_WO(x) ((x) + V_P_DIFF)    // same as P2V, but without casts
+
+#endif /* memlayout_h */
